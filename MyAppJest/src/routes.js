f7fc5077e02b37839584438d0664b0f49c92e410
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-07-06 01:19:36 -0300

    config

--- MyAppJest/src/routes.js
+++ MyAppJest/src/routes.js
@@ -1,4 +1,7 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
+
+User.create({ name: 'teste', email: 'teste@pop.com.br', password_hash: 'teste123' })
 
 
 module.exports = routes;
